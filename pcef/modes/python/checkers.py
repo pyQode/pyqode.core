45dc6a7f3a8d99a376424a38ebab47c54eac784a
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-03-06 23:25:29 +0100

    Refactor checker mode + add pylint and pyflakes checkers

--- pcef/modes/python/checkers.py
+++ pcef/modes/python/checkers.py
@@ -8,12 +8,14 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-from collections import deque
-import os
-from PySide.QtGui import QColor, QIcon
-from subprocess import Popen, PIPE, STDOUT
-from PySide.QtCore import QThread, Signal
 import sys
+
+from collections import deque
+from subprocess import Popen, PIPE, STDOUT
+
+from PySide.QtGui import QColor, QIcon
+from PySide.QtCore import QThread, Signal
+
 from pcef.panels.marker import Marker
 from pcef.core import Mode, TextDecoration, cursorForPosition
 
@@ -41,32 +43,46 @@ class CheckerThread(QThread):
             if len(self.__cmd_queue):
                 cmd = self.__cmd_queue.pop()
                 p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-                output = p.stdout.read()
-                self.outputAvailable.emit(output)
+                try:
+                    output = p.stdout.read()
+                    self.outputAvailable.emit(output)
+                except IOError:
+                    pass
             self.msleep(1)
 
 
-class Pep8CheckerMode(Mode, CheckerThread):
+# "PEP8", "Check for PEP8 violation on the fly"
 
-    def __init__(self):
-        super(Pep8CheckerMode, self).__init__("PEP8", "Check for PEP8 violation on the fly")
+#: Checker warning error type (pep8,...)
+ERROR_TYPE_WARNING = 0
+#: Checker syntax error type (pyflakes,...)
+ERROR_TYPE_SYNTAX = 1
+
+
+class CheckerMode(Mode, CheckerThread):
+
+    def __init__(self, name, description, base_cmd):
+        super(CheckerMode, self).__init__(name, description)
         CheckerThread.__init__(self)
         self.__decorations = []
         self.__markers = []
+        self.base_cmd = base_cmd
         self.checkers_panel = None
+        self.icons = {ERROR_TYPE_WARNING: ":/icons/rc/marker_warning.png",
+                      ERROR_TYPE_SYNTAX: ":/icons/rc/marker_error.png"}
+        self.colors = {ERROR_TYPE_WARNING: "#FF0000", ERROR_TYPE_SYNTAX: "#FFFF00"}
 
     def _onStateChanged(self, state):
         if state:
-            self.editor.codeEdit.textSaved.connect(self.__run_pep8)
-            self.editor.codeEdit.newTextSet.connect(self.__run_pep8)
+            self.editor.codeEdit.textSaved.connect(self.__run_cmd)
+            self.editor.codeEdit.newTextSet.connect(self.__run_cmd)
             self.outputAvailable.connect(self.__apply_results)
             if hasattr(self.editor, "checkers_panel"):
                 self.checkers_panel = self.editor.checkers_panel
-                print "Found checkers panel"
             self.start()
         else:
-            self.editor.codeEdit.textSaved.disconnect(self.__run_pep8)
-            self.editor.codeEdit.newTextSet.disconnect(self.__run_pep8)
+            self.editor.codeEdit.textSaved.disconnect(self.__run_cmd)
+            self.editor.codeEdit.newTextSet.disconnect(self.__run_cmd)
             self.outputAvailable.disconnect(self.__apply_results)
             self.checkers_panel = None
             self.is_running = False
@@ -86,6 +102,19 @@ class Pep8CheckerMode(Mode, CheckerThread):
                     pass
         self.__markers[:] = []
 
+    def addError(self, error_type, line, column=1, message=None, selectLine=True):
+        assert error_type in self.colors and self.colors[error_type] is not None
+        selectLine = selectLine or not column
+        c = cursorForPosition(self.editor.codeEdit, line, column, selectEndOfLine=selectLine)
+        deco = TextDecoration(c, draw_order=error_type + 1, tooltip=message)
+        deco.setSpellchecking(color=QColor(self.colors[error_type]))
+        self.__decorations.append(deco)
+        self.editor.codeEdit.addDecoration(deco)
+        if self.checkers_panel:
+            marker = Marker(line, icon=QIcon(self.icons[error_type]), tooltip=message)
+            self.__markers.append(marker)
+            self.checkers_panel.addMarker(marker)
+
     def __apply_results(self, raw_results):
         """
         Apply squiggle on all lines
@@ -95,10 +124,38 @@ class Pep8CheckerMode(Mode, CheckerThread):
         """
         self.__clear_decorations()
         self.__clear_markers()
-        lines = raw_results.splitlines()
         current_cursor = self.editor.codeEdit.textCursor()
         hbar_pos = self.editor.codeEdit.horizontalScrollBar().sliderPosition()
         vbar_pos = self.editor.codeEdit.verticalScrollBar().sliderPosition()
+        self.onResultsAvailable(raw_results)
+        self.editor.codeEdit.setTextCursor(current_cursor)
+        self.editor.codeEdit.horizontalScrollBar().setSliderPosition(hbar_pos)
+        self.editor.codeEdit.verticalScrollBar().setSliderPosition(vbar_pos)
+
+    def onResultsAvailable(self, raw_results):
+        raise NotImplementedError("The checker mode %s does not implement onResultsAvailable" % self.name)
+
+    def __run_cmd(self):
+        filename = self.editor.codeEdit.tagFilename
+        if filename:
+            cmd = "{0} {1}".format(self.base_cmd, self.editor.codeEdit.tagFilename)
+            self.execute(cmd)
+
+    def _onStyleChanged(self):
+        self.colors[ERROR_TYPE_WARNING] = QColor(self.editor.currentStyle.warningColor)
+        self.colors[ERROR_TYPE_SYNTAX] = QColor(self.editor.currentStyle.errorColor)
+        self.__run_cmd()
+
+
+class PEP8CheckerMode(CheckerMode):
+
+    def onResultsAvailable(self, raw_results):
+        """
+        Decode and apply PEP8 results to the editor
+
+        :param raw_results: the pep8 output string
+        """
+        lines = raw_results.splitlines()
         for line in lines:
             try:
                 offset = 0
@@ -107,35 +164,69 @@ class Pep8CheckerMode(Mode, CheckerThread):
                 tokens = line.split(':')
                 line_nr = int(tokens[1 + offset])
                 col_nr = int(tokens[2 + offset])
-                message = tokens[3 + offset]
-                c = cursorForPosition(self.editor.codeEdit, line_nr, col_nr, selectEndOfLine=True)
-                deco = TextDecoration(c, draw_order=1, tooltip="PEP8: %s" % message)
-                deco.setSpellchecking(color=self.color)
-                self.__decorations.append(deco)
-                self.editor.codeEdit.addDecoration(deco)
-
-                if self.checkers_panel:
-                    marker = Marker(line_nr, icon=QIcon(":/icons/rc/marker_warning.png"), tooltip="PEP8: %s" % message)
-                    self.__markers.append(marker)
-                    self.checkers_panel.addMarker(marker)
+                message = "PEP8: %s" % tokens[3 + offset]
+                self.addError(ERROR_TYPE_WARNING, line_nr, col_nr, message, True)
             except:
                 pass
-        self.editor.codeEdit.setTextCursor(current_cursor)
-        self.editor.codeEdit.horizontalScrollBar().setSliderPosition(hbar_pos)
-        self.editor.codeEdit.verticalScrollBar().setSliderPosition(vbar_pos)
 
-    def __run_pep8(self):
-        filename = self.editor.codeEdit.tagFilename
-        if filename:
-            cmd = "pep8 %s" % self.editor.codeEdit.tagFilename
-            self.execute(cmd)
-
-    def _onStyleChanged(self):
-        self.color = QColor(self.editor.currentStyle.warningColor)
-        self.__run_pep8()
+    def __init__(self):
+        super(PEP8CheckerMode, self).__init__(
+            "PEP8 Checker", "Check python code style using pep8.py", base_cmd="pep8")
 
 
-# mouse move
-# get cursor for position
-# check if any cursor extra selection contains the mouse cursor
-# if yes, show a tooltip
+class PyFlakesCheckerMode(CheckerMode):
+
+    def onResultsAvailable(self, raw_results):
+        """
+        Decode and apply PyFlakes results to the editor
+
+        :param raw_results: the pep8 output string
+        """
+        lines = raw_results.splitlines()
+        for line in lines:
+            try:
+                tokens = line.split(':')
+                nb_tokens = len(tokens)
+                line_nr = int(tokens[nb_tokens - 2])
+                message = "PyFlakes: %s" % tokens[nb_tokens - 1]
+                error_type = ERROR_TYPE_SYNTAX
+                # todo properly separate error messages from warning messages
+                if "used" in message:
+                    error_type = ERROR_TYPE_WARNING
+                self.addError(error_type, line_nr, column=1, message=message, selectLine=True)
+            except ValueError:
+                pass
+
+    def __init__(self):
+        super(PyFlakesCheckerMode, self).__init__(
+            "PyFlakes Checker", "Checks python syntax errors using pyflakes.py",
+            base_cmd="pyflakes")
+
+
+class PyLintCheckerMode(CheckerMode):
+
+    def onResultsAvailable(self, raw_results):
+        """
+        Decode and apply PyFlakes results to the editor
+
+        :param raw_results: the pep8 output string
+        """
+        lines = raw_results.splitlines()
+        for line in lines:
+            try:
+                tokens = line.split(':')
+                print tokens
+                nb_tokens = len(tokens)
+                line_nr = int(tokens[nb_tokens - 2])
+                message = "PyLint: %s" % tokens[nb_tokens - 1]
+                error_type = ERROR_TYPE_SYNTAX
+                if "[W" in message:
+                    error_type = ERROR_TYPE_WARNING
+                self.addError(error_type, line_nr, column=1, message=message, selectLine=True)
+            except ValueError:
+                pass
+
+    def __init__(self):
+        super(PyLintCheckerMode, self).__init__(
+            "Pylint Checker", "Checks python syntax errors using pylint.py",
+            base_cmd="pylint -f parseable -rn -dC,R")
