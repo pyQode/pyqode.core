8f9f1b5373207722f579a343403518964162cd3e
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-03-20 19:59:17 +0100

    Removed file handler (let the client use a file handler if he wants to)

--- pcef/__init__.py
+++ pcef/__init__.py
@@ -13,30 +13,27 @@ applications.
 
 This module contains helper functions for the end user.
 """
+import sys
 import logging
 from version import __version__ as pcef_version
 
 # create logger with 'spam_application'
 module_logger = logging.getLogger(__name__)
 module_logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('pcef.log')
-fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
-ch = logging.StreamHandler()
+ch = logging.StreamHandler("")
+ch.name = "pcef"
 ch.setLevel(logging.ERROR)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(levelname)s <%(name)s>: %(message)s')
-fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 # add the handlers to the logger
-module_logger.addHandler(fh)
 module_logger.info("#" * 80)
 module_logger.info("PCEF v{0}".format(pcef_version))
 module_logger.addHandler(ch)
 
 
-def openFileInEditor(editor, filename, encoding='utf8',
+def openFileInEditor(editor, filename, encoding=sys.getfilesystemencoding(),
                      replaceTabsBySpaces=True):
     """
     Open a file in an editor
@@ -62,7 +59,8 @@ def openFileInEditor(editor, filename, encoding='utf8',
     module_logger.info("File opened: {0}".format(filename))
 
 
-def saveFileFromEditor(editor, filename=None, encoding=None):
+def saveFileFromEditor(editor, filename=None,
+                       encoding=sys.getfilesystemencoding()):
     """
     Save the editor content to a file
 
