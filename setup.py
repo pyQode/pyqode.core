73a06c69249e20d1cb72d5f0119f0de24bbc7f10
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-03-22 15:55:39 +0100

    Fix packaging issues (dependies were not liste properly and failed to install automatically) Version is now 0.2.0-preview03

--- setup.py
+++ setup.py
@@ -20,9 +20,48 @@ from setuptools import setup, find_packages
 # properly get pcef version
 execfile('pcef/version.py')
 
+
 # get long description
 with open('README.rst', 'r') as readme:
     long_desc = readme.read()
+
+
+long_desc = """
+PySide Code Editing Framework
+=====================================
+
+PCEF is code editing framework for PySide applications.
+
+It provides a flexible code editor ready to use in any PySide
+applications. Flexibility is achieved through a system of editor extensions
+(custom panels and modes).
+
+In addition to the base widget, some preconfigured editors are available
+(a generic code editor and a python editor).
+
+Here are the core features:
+
+* **flexible framework** to add custom panels/modes
+* **syntax highlighting mode** (using pygments)
+* **code completion**
+* line number Panel
+* **code folding** Panel
+* markers Panel (to add breakpoints, bookmarks, errors,...)
+* right margin indicator mode
+* active line highlighting mode
+* **editor zoom** mode
+* find and replace Panel
+* **text decorations** (squiggle, box)
+* **easy styling** (built-in white and dark styles + possibility to customize
+  using **JSON style schemes**)
+* auto indent mode(indentation level is based on the previous line indent)
+
+Here are the python specific features:
+
+* code completion (using Jedi)
+* calltips
+* syntax and style checking (using pylint, pyflakes and pep8)
+"""
 
 
 setup(
@@ -38,9 +77,9 @@ setup(
     author_email='colin.duquesnoy@gmail.com',
     description='PySide Code Editing Framework (P.C.E.F.)',
     long_description=long_desc,
-    requires=['pygments', 'PySide', 'jedi', 'pep8', 'qdarkstyle'],
+    install_requires=['pygments', 'jedi', 'pep8', 'qdarkstyle', 'pylint',
+                      'pyflakes'],
     entry_points={'gui_scripts':
                   ['pcef_generic_example = examples.generic_example:main',
-                   'pcef_python_example = examples.python_example:main']}
-
+                   'pcef_python_example = examples.python_example:main']},
 )
