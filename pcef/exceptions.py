92d810378c6b23a88388177a8183e7987264e674
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-05-28 23:57:19 +0200

    Add exception module and move constants to the root level of pcef (so that exceptions and constants are defined in one place for all the PCEF API)

---
+++ pcef/exceptions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# PCEF - Python/Qt Code Editing Framework
+# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Contains the core exceptions classes
+"""
+
+
+class UnsupportedFeatureError(Exception):
+    """
+    This error is raised when a mode is not supported. This happens if you
+    try to use a specific mode which requires a specific library to be installed
+    (that is not part of the core requirements). Python modes are a good
+    example of mode who might raise this exception (all python have a
+    isSupported method that you can use when installing a mode).
+    """
+    pass
