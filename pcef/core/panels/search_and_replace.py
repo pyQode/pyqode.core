44f85513fa24e53189f249808348b659f0e5e22e
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-04 21:10:54 +0200

    Added search and replace ui + modify translate_ui for python3

--- pcef/core/panels/search_and_replace.py
+++ pcef/core/panels/search_and_replace.py
@@ -1,1 +1,19 @@
-__author__ = 'colin'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PCEF - Python/Qt Code Editing Framework
+# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+This module contains the search and replace panel
+"""
+class LineNumberPanel(Panel):
+    """
+    The liner number panel displays the document line numbers.
+    """
+    IDENTIFIER = "lineNumberArea"
+    DESCRIPTION = "Display line number"
--- pcef/core/ui/translate_ui.py
+++ pcef/core/ui/translate_ui.py
@@ -14,6 +14,23 @@ This script calls the PyQt ui compiler on all ui files found in the cwd.
 import glob
 import os
 
+
+def replace_rc_imports(filename):
+    with open(filename, "r") as f:
+        content = f.read()
+    new_content = ""
+    lines = content.splitlines()
+    for l in lines:
+        if l.startswith("import"):
+            to_replace = l.split(" ")[1]
+            replacement = "pcef.core.ui.{0}3".format(to_replace)
+            l = l.replace(to_replace, replacement)
+        new_content += "%s\n" % l
+    print(new_content)
+    with open(filename, "w") as f:
+        f.write(new_content)
+
+
 # compile ui files
 for name in glob.glob("*.ui"):
     base = name.split(".")[0]
@@ -21,6 +38,8 @@ for name in glob.glob("*.ui"):
     cmd = "pyuic4 -w {0} > {1}_ui3.py ".format(name, base)
     print(cmd)
     os.system(cmd)
+    replace_rc_imports("{0}_ui3.py".format(base))
+    # replace import *_rc by pcef.core.ui.*_rc3
     # python 2
     cmd = "pyuic4 {0} > {1}_ui.py ".format(name, base)
     print(cmd)
