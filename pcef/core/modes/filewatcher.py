70a642164d75ed75b403dce065ff63911f99931b
ogranada <ogranada@letmiapp.com>
2013-07-13 09:44:18 -0500

    Changes to FileWatcher

--- pcef/core/modes/filewatcher.py
+++ pcef/core/modes/filewatcher.py
@@ -1,7 +1,16 @@
 
-#!/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PCEF - Python/Qt Code Editing Framework
+# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 """
-module test...
+Contains the mode that control the external changes of file.
 """
 import hashlib
 import os
@@ -15,8 +24,9 @@ from pcef.core.mode import Mode
 
 class FileWatcher(Mode):
 
-    """"""
-
+    """
+    FileWatcher mode. (Verify the external changes from opened file)
+    """
     #: Mode identifier
     IDENTIFIER = "FileWatcher"
     #: Mode description
@@ -24,9 +34,10 @@ class FileWatcher(Mode):
 
     def __init__(self):
         super(FileWatcher, self).__init__()
-        self.jobRunner = JobRunner(self)
+        self.__filesystemwatcher = QtCore.QFileSystemWatcher()
         self.__textSum = ""
         self.__notify = True
+        self.__filesystemwatcher.fileChanged.connect(self.onFileChange)
 
     def onFileChange(self):
         # TODO: put i18n for this method
@@ -38,15 +49,18 @@ class FileWatcher(Mode):
             QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
         msgBox.setDefaultButton(QtGui.QMessageBox.No)
         ret = msgBox.exec_()
-        if ret == QtGui.QMessageBox.Save:
+        if ret == QtGui.QMessageBox.Yes:
             self.editor.openFile(self.editor.filePath)
         self.__notify = True
 
     @QtCore.Slot()
     def __setOriginalTextSum(self):
+        path = self.editor.filePath
         editorContent = str(
             self.editor.toPlainText()).encode()
         self.__textSum = hashlib.md5(editorContent).hexdigest()
+        if path not in self.__filesystemwatcher.files():
+            self.__filesystemwatcher.addPath(path)
 
     def onStateChanged(self, state):
         """
@@ -55,40 +69,9 @@ class FileWatcher(Mode):
         if state is True:
             self.editor.textSaved.connect(self.__setOriginalTextSum)
             self.editor.newTextSet.connect(self.__setOriginalTextSum)
-            self.jobRunner.startJob(
-                self.__compare, False, self.onFileChange)
             Mode.install(self, self.editor)
         else:
-            self.jobRunner.stopJob()
-
-    def __compare(self, callback, *args, **kwargs):
-        """
-        This function return the md5sum of file
-
-
-        :param filePath: filePath.
-        :type filePath: str
-
-        :param editor: editor.
-        :type editor: QTextEdit
-
-        :param callback: callback.
-        :type callback: callable
-
-        :param args: *args
-        :param kwargs: **kwargs
-        """
-        editor = self.editor
-        while True:
-            filePath = self.editor.filePath
-            if filePath and self.__notify:
-                with open(filePath, 'rb') as f:
-                    fileContent = f.read()
-                    fileContent = fileContent.replace(b"\r", b"")
-                originalSum = hashlib.md5(fileContent).hexdigest()
-                if self.__textSum != '' and self.__textSum != originalSum:
-                    callback(*args, **kwargs)
-            time.sleep(1)
+            self.__filesystemwatcher.removePath(self.editor.filePath)
 
 
 if __name__ == '__main__':
