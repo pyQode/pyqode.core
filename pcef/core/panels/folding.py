58beba2f235a3b1b5c3a4b04cdd47a8a4d2a2bfa
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-14 16:09:33 +0200

    Add fold scope highlighting in editor

--- pcef/core/panels/folding.py
+++ pcef/core/panels/folding.py
@@ -14,6 +14,7 @@ This module contains the marker panel
 from pcef.qt import QtCore, QtGui
 from pcef.core import constants
 from pcef.core.panel import Panel
+from pcef.core.decoration import TextDecoration
 from pcef.core.system import mergedColors, driftColor
 
 
@@ -52,18 +53,21 @@ class FoldingPanel(Panel):
         Panel.install(self, editor)
         self.__native = self.editor.style.addProperty("nativeFoldingIndicator",
                                                       True)
-        self.__color = self.editor.style.addProperty("foldScopeBackground",
+        self.__color = self.editor.style.addProperty("foldIndicatorBackground",
                                                      self.__systemColor)
+        self.__decoColor = driftColor(self.editor.style.value("panelBackground"))
 
     def onStyleChanged(self, section, key, value):
         Panel.onStyleChanged(self, section, key, value)
         if key == "nativeFoldingIndicator":
             self.__native = value
-        elif key == "foldScopeBackground":
+        elif key == "foldIndicatorBackground":
             self.__color = QtGui.QColor(value)
+        elif key == "panelBackground":
+            self.__decoColor = driftColor(QtGui.QColor(value))
 
     def resetScopeColor(self):
-        self.editor.style.setValue("foldScopeBackground", self.__systemColor)
+        self.editor.style.setValue("foldIndicatorBackground", self.__systemColor)
 
     def addIndicator(self, indicator):
         """
@@ -135,6 +139,32 @@ class FoldingPanel(Panel):
             else:
                 self.__rightArrowIcon[index].paint(painter, arrowRect)
 
+    def __drawBackgroundRect(self, foldZoneRect, painter):
+        c = self.__color
+        grad = QtGui.QLinearGradient(foldZoneRect.topLeft(),
+                                     foldZoneRect.topRight())
+        grad.setColorAt(0, c.lighter(110))
+        grad.setColorAt(1, c.lighter(130))
+        outline = c
+        painter.fillRect(foldZoneRect, grad)
+        painter.setPen(QtGui.QPen(outline))
+        painter.drawLine(foldZoneRect.topLeft() +
+                         QtCore.QPoint(1, 0),
+                         foldZoneRect.topRight() -
+                         QtCore.QPoint(1, 0))
+        painter.drawLine(foldZoneRect.bottomLeft() +
+                         QtCore.QPoint(1, 0),
+                         foldZoneRect.bottomRight() -
+                         QtCore.QPoint(1, 0))
+        painter.drawLine(foldZoneRect.topRight() +
+                         QtCore.QPoint(0, 1),
+                         foldZoneRect.bottomRight() -
+                         QtCore.QPoint(0, 1))
+        painter.drawLine(foldZoneRect.topLeft() +
+                         QtCore.QPoint(0, 1),
+                         foldZoneRect.bottomLeft() -
+                         QtCore.QPoint(0, 1))
+
     def paintEvent(self, event):
         Panel.paintEvent(self, event)
         painter = QtGui.QPainter(self)
@@ -150,37 +180,10 @@ class FoldingPanel(Panel):
                     h = self.sizeHint().height()
                 foldZoneRect = QtCore.QRect(
                     0, top, self.sizeHint().width(), h)
-                # Draw Foldable zone rect
-                active = False
                 expanded = indic.state == FoldingIndicator.UNFOLDED
-                if indic == self.__mouseOveredIndic:
-                    active = True
-                    c = self.__color
-                    grad = QtGui.QLinearGradient(foldZoneRect.topLeft(),
-                                                 foldZoneRect.topRight())
-                    grad.setColorAt(0, c.lighter(110))
-                    grad.setColorAt(1, c.lighter(130))
-                    outline = c
-                    painter.fillRect(foldZoneRect, grad)
-                    painter.setPen(QtGui.QPen(outline))
-                    painter.drawLine(foldZoneRect.topLeft() +
-                                     QtCore.QPoint(1, 0),
-                                     foldZoneRect.topRight() -
-                                     QtCore.QPoint(1, 0))
-                    painter.drawLine(foldZoneRect.bottomLeft() +
-                                     QtCore.QPoint(1, 0),
-                                     foldZoneRect.bottomRight() -
-                                     QtCore.QPoint(1, 0))
-                    painter.drawLine(foldZoneRect.topRight() +
-                                     QtCore.QPoint(0, 1),
-                                     foldZoneRect.bottomRight() -
-                                     QtCore.QPoint(0, 1))
-                    painter.drawLine(foldZoneRect.topLeft() +
-                                     QtCore.QPoint(0, 1),
-                                     foldZoneRect.bottomLeft() -
-                                     QtCore.QPoint(0, 1))
-
-                # draw arrow
+                active = indic == self.__mouseOveredIndic
+                if active:
+                    self.__drawBackgroundRect(foldZoneRect, painter)
                 self.drawArrow(arrowRect, active, expanded, painter)
 
     def __clearDecorations(self):
@@ -189,6 +192,25 @@ class FoldingPanel(Panel):
         self.__decorations[:] = []
         return
 
+    def __addDecorationsForIndic(self, indic):
+        self.__mouseOveredIndic = indic
+        tc = self.editor.textCursor()
+        tc.movePosition(tc.Start, tc.MoveAnchor)
+        tc.movePosition(tc.Down, tc.KeepAnchor, indic.start - 1)
+        d = TextDecoration(tc)
+        d.setFullWidth(True, clear=False)
+        d.setBackground(self.__decoColor)
+        self.editor.addDecoration(d)
+        self.__decorations.append(d)
+        tc.movePosition(tc.Start, tc.MoveAnchor)
+        tc.movePosition(tc.Down, tc.MoveAnchor, indic.end)
+        tc.movePosition(tc.End, tc.KeepAnchor)
+        d = TextDecoration(tc)
+        d.setFullWidth(True, clear=False)
+        d.setBackground(self.__decoColor)
+        self.editor.addDecoration(d)
+        self.__decorations.append(d)
+
     def mouseMoveEvent(self, event):
         line = self.editor.lineNumber(event.pos().y())
         if not line:
@@ -196,11 +218,16 @@ class FoldingPanel(Panel):
             return
         indic = self.getNearestIndicator(line)
         if indic != self.__mouseOveredIndic:
-            self.__mouseOveredIndic = indic
+            self.__clearDecorations()
+            if not indic:
+                self.repaint()
+                return
+            self.__addDecorationsForIndic(indic)
             self.repaint()
 
     def leaveEvent(self, e):
         self.__mouseOveredIndic = None
+        self.__clearDecorations()
         self.repaint()
 
 
