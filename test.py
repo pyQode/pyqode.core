634fee8487b4d68221ba478aebe2c29325c99fa9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-15 15:20:49 +0200

    Try "virtualenv: system_site_packages: true"

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ version (2.7, 3.2, 3.3).
 It runs a QApplication and shows a QGenericCodeEdit for 500ms.
 """
 import sys
-import os
-if sys.version_info[0] == 3:
-    sys.path.append("/usr/lib/python3.3/dist-packages")
-    sys.path.append("/usr/local/lib/python3.3/dist-packages")
-    sys.path.append("/usr/lib/python3/dist-packages")
-elif sys.version_info[0] == 2:
-    sys.path.append("/usr/lib/python2.7/dist-packages")
-    sys.path.append("/usr/local/lib/python2.7/dist-packages")
-    sys.path.append("/usr/lib/python2/dist-packages")
-print(sys.path)
+# import os
+# if sys.version_info[0] == 3:
+#     sys.path.append("/usr/lib/python3.3/dist-packages")
+#     sys.path.append("/usr/local/lib/python3.3/dist-packages")
+#     sys.path.append("/usr/lib/python3/dist-packages")
+# elif sys.version_info[0] == 2:
+#     sys.path.append("/usr/lib/python2.7/dist-packages")
+#     sys.path.append("/usr/local/lib/python2.7/dist-packages")
+#     sys.path.append("/usr/lib/python2/dist-packages")
+# print(sys.path)
 from pcef.qt import QtCore, QtGui
 from pcef.core import QGenericCodeEdit
 
