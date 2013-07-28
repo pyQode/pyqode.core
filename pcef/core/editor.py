da60b94440861c71622cca73f4cf43aed9f51c62
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-07-28 14:44:13 +0200

    Add missing license headers

--- pcef/core/editor.py
+++ pcef/core/editor.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PCEF - Python/Qt Code Editing Framework
+# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 """
 This module contains the definition of the QCodeEdit
 """
--- pcef/core/modes/indenter.py
+++ pcef/core/modes/indenter.py
@@ -8,16 +8,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# PCEF - Python/Qt Code Editing Framework
-# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
-#
-# This software is released under the LGPLv3 license.
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
 """ Contains the automatic generic indenter """
 import re
 from pcef.core.mode import Mode
