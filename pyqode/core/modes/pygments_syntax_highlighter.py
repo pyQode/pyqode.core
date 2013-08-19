957a0b5156745f7809f8a24a7ba5d3a894c8f4e4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-08-19 15:42:22 +0200

    Fix source code license header

--- pyqode/core/modes/pygments_syntax_highlighter.py
+++ pyqode/core/modes/pygments_syntax_highlighter.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# pyQode - Python/Qt Code Editor widget
-# Copyright 2013, Colin Duquesnoy <colin.duquesnoy@gmail.com>
+# Copyright 2013 Colin Duquesnoy
 #
-# This software is released under the LGPLv3 license.
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# This file is part of pyQode.
+#
+# pyQode is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pyQode is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with pyQode. If not, see http://www.gnu.org/licenses/.
 #
 """
 This module contains Syntax Highlighting mode and the QSyntaxHighlighter based
