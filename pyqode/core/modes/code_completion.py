effa515225cbae72a97a4329af0a3c9b4b5792f4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-09-01 18:10:57 +0200

    Fix travis build

--- pyqode/core/modes/code_completion.py
+++ pyqode/core/modes/code_completion.py
@@ -493,9 +493,9 @@ class CodeCompletionMode(Mode, QtCore.QObject):
                               unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
                               unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff))
             else:
-                RE_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
-                             u'|' + \
-                             u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
+                RE_ILLEGAL = '([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
+                             '|' + \
+                             '([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
                              (chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff),
                               chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff),
                               chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff))
