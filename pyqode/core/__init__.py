01cd8e65ca7521585ddea3e05d7bfc9742902704
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-10-04 18:03:00 +0200

    #25 still trying

--- pyqode/core/__init__.py
+++ pyqode/core/__init__.py
@@ -32,8 +32,8 @@ code editor widget
 #
 
 # modules
-from pyqode.core import constants
-from pyqode.core import logger
+#from pyqode.core import constants
+#from pyqode.core import logger
 
 # core classes
 from pyqode.core.editor import QCodeEdit
