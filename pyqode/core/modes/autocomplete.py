59bb7bf2e1a6b4e4d73bfa7d11fa87251cb3f0e6
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-03-24 09:03:29 +0100

    A few more improvements to autocomplete

--- pyqode/core/modes/autocomplete.py
+++ pyqode/core/modes/autocomplete.py
@@ -68,10 +68,6 @@ class AutoCompleteMode(Mode):
             next_char = None
         if txt in self.MAPPING:
             toInsert = self.MAPPING[txt]
-            if next_char == toInsert:
-                tc.clearSelection()
-                self.editor.setTextCursor(tc)
-                return
             if (not next_char or next_char in self.MAPPING.keys() or
                     next_char in self.MAPPING.values() or
                     next_char.isspace()):
