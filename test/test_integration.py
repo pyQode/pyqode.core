e29f3e93d541271a346fde6c3e287ba7778b7a01
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-04-15 14:45:00 +0200

    Improve test integration: install most modes/panels

--- test/test_integration.py
+++ test/test_integration.py
@@ -10,6 +10,7 @@ import logging
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 from pyqode.core import frontend
+from pyqode.core.frontend import modes, panels
 from .helpers import cwd_at
 
 
@@ -32,6 +33,26 @@ def test_app():
     app = QtGui.QApplication(sys.argv)
     editor = frontend.QCodeEdit()
     frontend.start_server(editor, os.path.join(os.getcwd(), 'server.py'))
+
+    # install the same modes/panels as in the simple_editor example
+    # add panels
+    frontend.install_panel(editor, panels.LineNumberPanel())
+    frontend.install_panel(editor, panels.SearchAndReplacePanel(),
+                           panels.SearchAndReplacePanel.Position.BOTTOM)
+    # add modes
+    frontend.install_mode(editor, modes.AutoCompleteMode())
+    frontend.install_mode(editor, modes.CaseConverterMode())
+    frontend.install_mode(editor, modes.FileWatcherMode())
+    frontend.install_mode(editor, modes.CaretLineHighlighterMode())
+    frontend.install_mode(editor, modes.RightMarginMode())
+    frontend.install_mode(editor, modes.PygmentsSyntaxHighlighter(
+        editor.document()))
+    frontend.install_mode(editor, modes.ZoomMode())
+    frontend.install_mode(editor, modes.CodeCompletionMode())
+    frontend.install_mode(editor, modes.AutoIndentMode())
+    frontend.install_mode(editor, modes.IndenterMode())
+    frontend.install_mode(editor, modes.SymbolMatcherMode())
+
     frontend.open_file(editor, __file__)
     # editor.show()
     QtCore.QTimer.singleShot(2000, _leave)
