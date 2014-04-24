f27e0293e1e8cadbb536671a379ca1ef028041d9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-04-24 11:32:25 +0200

    #123 Integrates InteractiveConsole

---
+++ pyqode/core/frontend/widgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+This package contains a set of widgets that might be useful when writing
+pyqode applications:
+
+    - InteractiveConsole: QTextEdit made for running background process
+      interactively. Can be used in an IDE for running programs or to display
+      the compiler output,...
+
+    - CodeEditTabWidget: tab widget made to handle CodeEdit instances (or
+      any other object that have the same interface).
+
+    - ErrorsTable: a QTableWidget specialised to show CheckerMessage.
+
+    - QPropertyGrid: a simple property grid that works on pure python object
+    made up of primitive types (int, float, list, string).
+
+"""
