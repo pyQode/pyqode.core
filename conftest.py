72d51e8ec7b6858c96e428ee8f538303593cbbb3
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-05-06 15:22:57 +0200

    conftest: setup debug log level

--- conftest.py
+++ conftest.py
@@ -7,24 +7,12 @@ This scripts configures the test suite. We do two things:
       this implies that you must use **QApplication.instance** in your
       test scripts.
 """
-import pytest
-from PyQt4.QtGui import QApplication
 import sys
+from PyQt4.QtGui import QApplication
 
 # 1. setup logging
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 # 2. create qt application
 app = QApplication(sys.argv)
-
-
-# def teardown():
-#     global app
-#     app = None
-#     del app
-#
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def create_qapplication(request):
-#     request.addfinalizer(teardown)
