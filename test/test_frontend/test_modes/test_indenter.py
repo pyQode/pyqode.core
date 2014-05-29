fadf56666cdf6ec3c45a0c332e9b7da0daf8a54c
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-05-29 23:05:06 +0200

    Reproduce bug with unindent
    unindent eats meaningful characters instead of spaces

--- test/test_frontend/test_modes/test_indenter.py
+++ test/test_frontend/test_modes/test_indenter.py
@@ -22,3 +22,16 @@ def test_indent_selection(editor):
     frontend.select_lines(editor)
     mode.indent()
     mode.unindent()
+
+
+
+
+def test_bug_unindent(editor):
+    editor.clear()
+    # empty doc
+    mode = frontend.get_mode(editor, modes.IndenterMode)
+    editor.clear()
+    editor.setPlainText('    print("foo")', 'text/x-python', 'utf-8')
+    frontend.goto_line(editor, 1, column=len('    print("foo")'))
+    mode.unindent()
+    assert editor.toPlainText() == 'print("foo")'
