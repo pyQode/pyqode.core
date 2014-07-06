1c9d50c5dbbf6b612e2e59764e4ef8f9eef8cafa
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-07-07 01:20:11 +0200

    close #137

--- pyqode/core/widgets/__init__.py
+++ pyqode/core/widgets/__init__.py
@@ -3,14 +3,16 @@
 This package contains a set of widgets that might be useful when writing
 pyqode applications:
 
+    - TextEdit: code edit specialised for plain text
+    - GenericCodeEdit: generic code edit, using PygmentsSH.
+      Not really fast, not really smart.
     - InteractiveConsole: QTextEdit made for running background process
       interactively. Can be used in an IDE for running programs or to display
       the compiler output,...
     - CodeEditTabWidget: tab widget made to handle CodeEdit instances (or
       any other object that have the same interface).
     - ErrorsTable: a QTableWidget specialised to show CheckerMessage.
-    - QPropertyGrid: a simple property grid that works on pure python object
-      made up of primitive types (int, float, list, string).
+
 
 """
 # pylint: disable=unused-import
