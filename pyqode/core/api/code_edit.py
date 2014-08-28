b940022043ebd2adf1856079687a3b3cfa33f785
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-28 11:04:32 +0200

    Fix a visual bug where horizontal scrollbars range is not correct
    This usually happen if the editor was resized while being hidden

--- pyqode/core/api/code_edit.py
+++ pyqode/core/api/code_edit.py
@@ -629,6 +629,16 @@ class CodeEdit(QtWidgets.QPlainTextEdit):
 
         :param event: QFocusEvent
         """
+        # fix a visual bug if the editor was resized while being hidden (
+        # e.g. a dock # widget has been resized and some editors were in a
+        # tab widget. Non visible editor have a visual bug where horizontal
+        # scroll bar range
+        #
+        TextHelper(self).mark_whole_doc_dirty()
+        s = self.size()
+        s.setWidth(s.width() + 1)
+        self.resizeEvent(QtGui.QResizeEvent(self.size(), s))
+
         self.focused_in.emit(event)
         super().focusInEvent(event)
         self.repaint()
