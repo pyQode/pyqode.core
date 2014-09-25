ebb421a11776782ae1be120c2a0f59ef99375f4c
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-09-25 19:12:56 +0200

    Fix panels size (right panels was visible under bottom panel)

--- pyqode/core/managers/panels.py
+++ pyqode/core/managers/panels.py
@@ -131,13 +131,13 @@ class PanelsManager(Manager):
 
     def resize(self):
         """ Resizes panels """
-        content_rect = self.editor.contentsRect()
-        viewport_content_rect = self.editor.viewport().contentsRect()
+        crect = self.editor.contentsRect()
+        view_crect = self.editor.viewport().contentsRect()
         s_bottom, s_left, s_right, s_top = self._compute_zones_sizes()
-        w_offset = content_rect.width() - (viewport_content_rect.width() +
-                                           s_left + s_right)
-        h_offset = content_rect.height() - (viewport_content_rect.height() +
-                                            s_bottom + s_top)
+        tw = s_left + s_right
+        th = s_bottom + s_top
+        w_offset = crect.width() - (view_crect.width() + tw)
+        h_offset = crect.height() - (view_crect.height() + th)
         left = 0
         panels = self.panels_for_zone(Panel.Position.LEFT)
         panels.sort(key=lambda panel: panel.order_in_zone, reverse=True)
@@ -146,11 +146,10 @@ class PanelsManager(Manager):
                 continue
             panel.adjustSize()
             size_hint = panel.sizeHint()
-            panel.setGeometry(content_rect.left() + left,
-                              content_rect.top() + s_top,
+            panel.setGeometry(crect.left() + left,
+                              crect.top() + s_top,
                               size_hint.width(),
-                              content_rect.height() - s_bottom - s_top -
-                              h_offset)
+                              crect.height() - s_bottom - s_top - h_offset)
             left += size_hint.width()
         right = 0
         panels = self.panels_for_zone(Panel.Position.RIGHT)
@@ -159,10 +158,11 @@ class PanelsManager(Manager):
             if not panel.isVisible():
                 continue
             size_hint = panel.sizeHint()
-            panel.setGeometry(content_rect.right() - right -
-                              size_hint.width() - w_offset,
-                              content_rect.top() + s_top, size_hint.width(),
-                              content_rect.height() - h_offset)
+            panel.setGeometry(
+                crect.right() - right - size_hint.width() - w_offset,
+                crect.top() + s_top,
+                size_hint.width(),
+                crect.height() - s_bottom - s_top - h_offset)
             right += size_hint.width()
         top = 0
         panels = self.panels_for_zone(Panel.Position.TOP)
@@ -171,8 +171,9 @@ class PanelsManager(Manager):
             if not panel.isVisible():
                 continue
             size_hint = panel.sizeHint()
-            panel.setGeometry(content_rect.left(), content_rect.top() + top,
-                              content_rect.width() - w_offset,
+            panel.setGeometry(crect.left(),
+                              crect.top() + top,
+                              crect.width() - w_offset,
                               size_hint.height())
             top += size_hint.height()
         bottom = 0
@@ -182,11 +183,11 @@ class PanelsManager(Manager):
             if not panel.isVisible():
                 continue
             size_hint = panel.sizeHint()
-            panel.setGeometry(content_rect.left(),
-                              content_rect.bottom() - bottom -
-                              size_hint.height() - h_offset,
-                              content_rect.width() - w_offset,
-                              size_hint.height())
+            panel.setGeometry(
+                crect.left(),
+                crect.bottom() - bottom - size_hint.height() - h_offset,
+                crect.width() - w_offset,
+                size_hint.height())
             bottom += size_hint.height()
 
     def update(self, rect, delta_y, force_update_margins=False):
@@ -209,7 +210,6 @@ class PanelsManager(Manager):
                 self._cached_cursor_pos = helper.cursor_position()
         if (rect.contains(self.editor.viewport().rect()) or
                 force_update_margins):
-            # _logger().debug('_update_panels -> _update_viewport_margins')
             self._update_viewport_margins()
 
     def _update_viewport_margins(self):
@@ -218,43 +218,24 @@ class PanelsManager(Manager):
         left = 0
         right = 0
         bottom = 0
-        # _logger().debug('updating viewport margins')
-        # _logger().debug('processing left panels')
         for panel in self.panels_for_zone(Panel.Position.LEFT):
             if panel.isVisible():
                 width = panel.sizeHint().width()
-                # _logger().debug('right panel width: %r', width)
                 left += width
-            # else:
-                # _logger().debug('skipping invisible panel %r', panel.name)
-        # _logger().debug('processing right panels')
         for panel in self.panels_for_zone(Panel.Position.RIGHT):
             if panel.isVisible():
                 width = panel.sizeHint().width()
-                # _logger().debug('right panel width: %r', width)
                 right += width
-            # else:
-                # _logger().debug('skipping invisible panel %s', panel.name)
-        # _logger().debug('processing top panels')
         for panel in self.panels_for_zone(Panel.Position.TOP):
             if panel.isVisible():
                 height = panel.sizeHint().height()
-                # _logger().debug('_bottom panel height: %r', height)
                 top += height
-            # else:
-                # _logger().debug('skipping invisible panel %s', panel.name)
-        # _logger().debug('processing _bottom panels')
         for panel in self.panels_for_zone(Panel.Position.BOTTOM):
             if panel.isVisible():
                 height = panel.sizeHint().height()
-                # _logger().debug('_bottom panel height: %r', height)
                 bottom += height
-            # else:
-                # _logger().debug('skipping invisible panel %s', panel.name)
         self._margin_sizes = (top, left, right, bottom)
         self.editor.setViewportMargins(left, top, right, bottom)
-        # _logger().debug('viewport margins updated: %r', repr(
-        #     self._margin_sizes))
 
     def margin_size(self, position=Panel.Position.LEFT):
         """
