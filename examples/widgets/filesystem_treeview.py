d30c191a312ed8896bee470ec0c8d291959bfeba
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-10-29 14:57:55 +0100

    Add FileSystemTreeView (with basic file system operations: copy, cut, paste, deleted, copy path). Move the widgets examples in a dedicated directory Add a test directory structure for playing with the FileSystemTreeView

---
+++ examples/widgets/filesystem_treeview.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import sys
+import pyqode.core
+from pyqode.core.widgets import FileSystemTreeView, FileSystemContextMenu
+from pyqode.qt import QtWidgets
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+    def _on_tv_activated(index):
+        print(tv.filePath(index))
+        print(tv.fileInfo(index))
+
+    app = QtWidgets.QApplication(sys.argv)
+    tv = FileSystemTreeView()
+    tv.set_context_menu(FileSystemContextMenu())
+    tv.activated.connect(_on_tv_activated)
+    tv.setHeaderHidden(True)
+    for i in range(1, 4):
+        tv.hideColumn(i)
+    path = os.path.join(os.getcwd(), 'test_dir')
+    print('watching: %s' % path)
+    tv.set_root_path(path)
+    tv.show()
+    app.exec_()
