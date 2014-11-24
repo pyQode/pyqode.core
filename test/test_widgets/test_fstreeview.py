834de5fb0ea6b5acb51d01f51a4ff523f488d68b
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-24 16:24:27 +0100

    Temporarily remove test widgets except test_recents which is failing on travis but not on my locale machine

--- test/test_widgets/test_fstreeview.py
+++ test/test_widgets/test_fstreeview.py
@@ -1,13 +1,13 @@
-import os
-from pyqode.core.widgets import FileSystemTreeView, FileSystemContextMenu
-from pyqode.qt.QtTest import QTest
-
-
-def test_fs_treeview():
-    tv = FileSystemTreeView()
-    tv.set_context_menu(FileSystemContextMenu())
-    tv.set_root_path(__file__)
-    tv.show()
-    QTest.qWait(500)
-    tv.close()
-    del tv
+# import os
+# from pyqode.core.widgets import FileSystemTreeView, FileSystemContextMenu
+# from pyqode.qt.QtTest import QTest
+#
+#
+# def test_fs_treeview():
+#     tv = FileSystemTreeView()
+#     tv.set_context_menu(FileSystemContextMenu())
+#     tv.set_root_path(__file__)
+#     tv.show()
+#     QTest.qWait(500)
+#     tv.close()
+#     del tv
