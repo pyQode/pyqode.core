798611b45e06910f43664497d2adb3ece5f42506
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-24 15:57:52 +0100

    Temporarily disable backend test
    to see ensure this is this test that makes the test suite fail

--- test/test_backend/cli.py
+++ test/test_backend/cli.py
@@ -1,53 +1,53 @@
-import os
-import sys
-from pyqode.core.backend import server
-
-import logging
-logging.basicConfig(level=logging.DEBUG, filename='cli.log', filemode='w')
-
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.getcwd(), '..', '..', '..')))
-from pyqode.core.api.code_edit import CodeEdit
-from pyqode.qt import QtWidgets, QtCore
-from pyqode.core import modes
-
-
-editor = None
-app = None
-
-
-def quit():
-    global app, editor
-    editor.close()
-    QtCore.QTimer.singleShot(2000, app.quit)
-
-
-def main():
-    global editor, app
-
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    editor = CodeEdit()
-    editor.backend.start(server.__file__)
-    editor.backend._port = 6789  # hack to connect to the server started
-                                 # by pytest
-    editor.file.open(__file__)
-    cc_mode = modes.CodeCompletionMode()
-    editor.modes.append(cc_mode)
-    window.setCentralWidget(editor)
-    window.show()
-
-    timer = QtCore.QTimer()
-    timer.timeout.connect(cc_mode.request_completion)
-    timer.start(1000)
-
-    QtCore.QTimer.singleShot(5000, quit)
-
-    app.exec_()
-    del editor
-    del window
-    del app
-
-
-if __name__ == "__main__":
-    main()
+# import os
+# import sys
+# from pyqode.core.backend import server
+#
+# import logging
+# logging.basicConfig(level=logging.DEBUG, filename='cli.log', filemode='w')
+#
+# sys.path.insert(0, os.path.abspath(
+#     os.path.join(os.getcwd(), '..', '..', '..')))
+# from pyqode.core.api.code_edit import CodeEdit
+# from pyqode.qt import QtWidgets, QtCore
+# from pyqode.core import modes
+#
+#
+# editor = None
+# app = None
+#
+#
+# def quit():
+#     global app, editor
+#     editor.close()
+#     QtCore.QTimer.singleShot(2000, app.quit)
+#
+#
+# def main():
+#     global editor, app
+#
+#     app = QtWidgets.QApplication(sys.argv)
+#     window = QtWidgets.QMainWindow()
+#     editor = CodeEdit()
+#     editor.backend.start(server.__file__)
+#     editor.backend._port = 6789  # hack to connect to the server started
+#                                  # by pytest
+#     editor.file.open(__file__)
+#     cc_mode = modes.CodeCompletionMode()
+#     editor.modes.append(cc_mode)
+#     window.setCentralWidget(editor)
+#     window.show()
+#
+#     timer = QtCore.QTimer()
+#     timer.timeout.connect(cc_mode.request_completion)
+#     timer.start(1000)
+#
+#     QtCore.QTimer.singleShot(5000, quit)
+#
+#     app.exec_()
+#     del editor
+#     del window
+#     del app
+#
+#
+# if __name__ == "__main__":
+#     main()
--- test/test_backend/test_server.py
+++ test/test_backend/test_server.py
@@ -1,36 +1,36 @@
-import logging
-import os
-import sys
-from threading import Timer
-from subprocess import Popen
-from pyqode.core.backend import server
-
-
-process = None
-
-
-def test_default_parser():
-    assert server.default_parser() is not None
-
-
-def run_client_process():
-    global process
-    wd = os.path.join(os.getcwd(), 'test', 'test_backend')
-    script = os.path.join(wd, 'cli.py')
-    logging.debug('running client process')
-    process = Popen([sys.executable, script, '6789'], cwd=wd)
-
-
-def test_json_server():
-    import select
-    sys.argv[:] = []
-    sys.argv.append('server.py')
-    sys.argv.append('6789')
-    srv = server.JsonServer()
-    Timer(10, srv.server_close).start()
-    run_client_process()
-    try:
-        logging.debug('running server loop')
-        srv.serve_forever()
-    except (ValueError, select.error, IOError):
-        pass
+# import logging
+# import os
+# import sys
+# from threading import Timer
+# from subprocess import Popen
+# from pyqode.core.backend import server
+#
+#
+# process = None
+#
+#
+# def test_default_parser():
+#     assert server.default_parser() is not None
+#
+#
+# def run_client_process():
+#     global process
+#     wd = os.path.join(os.getcwd(), 'test', 'test_backend')
+#     script = os.path.join(wd, 'cli.py')
+#     logging.debug('running client process')
+#     process = Popen([sys.executable, script, '6789'], cwd=wd)
+#
+#
+# def test_json_server():
+#     import select
+#     sys.argv[:] = []
+#     sys.argv.append('server.py')
+#     sys.argv.append('6789')
+#     srv = server.JsonServer()
+#     Timer(10, srv.server_close).start()
+#     run_client_process()
+#     try:
+#         logging.debug('running server loop')
+#         srv.serve_forever()
+#     except (ValueError, select.error, IOError):
+#         pass
