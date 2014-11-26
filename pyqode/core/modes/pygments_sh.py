80ff1840c1615f0d4adb2d2f144ea8acbb60bdd9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-11-26 10:19:29 +0100

    Simplify monkeypatch

--- pyqode/core/modes/pygments_sh.py
+++ pyqode/core/modes/pygments_sh.py
@@ -50,25 +50,19 @@ def get_tokens_unprocessed(self, text, stack=('root',)):
         statestack = list(self._saved_state_stack)
     else:
         statestack = list(stack)
-    try:
-        statetokens = tokendefs[statestack[-1]]
-    except KeyError:
-        self._saved_state_stack = None
-        return
+    statetokens = tokendefs[statestack[-1]]
     while 1:
         for rexmatch, action, new_state in statetokens:
-            match = rexmatch(text, pos)
-            if match:
-                try:
+            m = rexmatch(text, pos)
+            if m:
+                if action is not None:
                     if type(action) is _TokenType:
-                        yield pos, action, match.group()
+                        yield pos, action, m.group()
                     else:
-                        for item in action(self, match):
+                        for item in action(self, m):
                             yield item
-                except TypeError:
-                    pass
-                pos = match.end()
-                if new_state is not None or action is None:
+                pos = m.end()
+                if new_state is not None:
                     # state transition
                     if isinstance(new_state, tuple):
                         for state in new_state:
@@ -94,7 +88,7 @@ def get_tokens_unprocessed(self, text, stack=('root',)):
                     pos += 1
                     statestack = ['root']
                     statetokens = tokendefs['root']
-                    yield pos, Text, '\n'
+                    yield pos, Text, u'\n'
                     continue
                 yield pos, Error, text[pos]
                 pos += 1
