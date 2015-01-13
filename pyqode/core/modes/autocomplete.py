330544d369f84c7328740e8d7e0b9068824e04ea
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-01-13 15:13:34 +0100

    Fix autocomplete of backspace
    Backspace should remove the correponding character if next char is not empty and is in the mapping.  E.g.: (|), pressing delete at | should remove both parentheses

--- pyqode/core/modes/autocomplete.py
+++ pyqode/core/modes/autocomplete.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """ Contains the AutoCompleteMode """
 import logging
+from pyqode.qt import QtCore
 from pyqode.core.api import TextHelper
 from pyqode.core.api.mode import Mode
 
@@ -71,7 +72,19 @@ class AutoCompleteMode(Mode):
         next_char = TextHelper(self.editor).get_right_character()
         self.logger.debug('next char: %s', next_char)
         ignore = False
-        if txt and next_char == txt and next_char in self.MAPPING:
+        if event.key() == QtCore.Qt.Key_Backspace:
+            # get the character that will get deleted
+            tc = self.editor.textCursor()
+            tc.movePosition(tc.PreviousCharacter)
+            tc.movePosition(tc.NextCharacter, tc.KeepAnchor)
+            del_char = tc.selectedText()
+            if self.MAPPING[del_char] == next_char:
+                tc.beginEditBlock()
+                tc.movePosition(tc.NextCharacter, tc.KeepAnchor, 2)
+                tc.removeSelectedText()
+                tc.endEditBlock()
+                ignore = True
+        elif txt and next_char == txt and next_char in self.MAPPING:
             ignore = True
         elif event.text() == ')' or event.text() == ']' or event.text() == '}':
             if next_char == ')' or next_char == ']' or next_char == '}':
