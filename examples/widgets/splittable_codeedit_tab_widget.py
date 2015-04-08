bae927e17e8c9cafc0652f58fd08c2121087ef6e
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-04-08 11:15:25 +0200

    SplittableTabWidget: add ability to add custom context menu actions

--- examples/widgets/splittable_codeedit_tab_widget.py
+++ examples/widgets/splittable_codeedit_tab_widget.py
@@ -1,12 +1,19 @@
-import os
 import sys
 from pyqode.qt import QtWidgets
 from pyqode.core.widgets import SplittableCodeEditTabWidget
 
 
+def open_in_explorer():
+    print('open %r in explorer' %
+          SplittableCodeEditTabWidget.tab_under_menu.file.path)
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     tab_widget = SplittableCodeEditTabWidget()
+    action = QtWidgets.QAction('Open in explorer', tab_widget)
+    action.triggered.connect(open_in_explorer)
+    tab_widget.add_context_action(action)
     tab_widget.setMinimumSize(800, 600)
     tab_widget.open_document(__file__)
     tab_widget.open_document(QtWidgets.__file__)
