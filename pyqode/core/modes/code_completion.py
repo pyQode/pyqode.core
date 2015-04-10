e5460d68921cc43a7cea4909217731eb9116cfb0
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-04-10 10:42:34 +0200

    Improve subsequence matching
    at least two consecutive letters are required (reverted from commit c01af7865778d05f279b1723b8a28876f75761e1)

--- pyqode/core/modes/code_completion.py
+++ pyqode/core/modes/code_completion.py
@@ -21,7 +21,6 @@ class SmarSortFilterProxyModel(QtCore.QSortFilterProxyModel):
         tl = self.sourceModel().data(left).lower()
         tr = self.sourceModel().data(right).lower()
         pattern = self.filterRegExp()
-        print(pattern.indexIn(tl), tl, pattern.indexIn(tr), tr)
         return pattern.indexIn(tl) < pattern.indexIn(tr)
 
 
@@ -49,12 +48,7 @@ class SmartCompleter(QtWidgets.QCompleter):
             self.filterProxyModel.setSourceModel(self.source_model)
 
         self.filterProxyModel.invalidate()  # force sorting/filtering
-        pattern = []
-        for i in range(len(self.local_completion_prefix)):
-            if i != 0 and i % 2 == 0:
-                pattern.append('.*')
-            pattern.append(self.local_completion_prefix[i])
-        pattern = ''.join(pattern)
+        pattern = ''.join([x + '.*' for x in self.local_completion_prefix])
         pattern = QtCore.QRegExp(
             pattern, self.caseSensitivity(), QtCore.QRegExp.RegExp)
 
