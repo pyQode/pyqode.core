ff1f652aa0859d00ed15f943673ecf4f50584470
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-05-27 20:26:52 +0200

    Fix clear modes on big files

--- pyqode/core/managers/file.py
+++ pyqode/core/managers/file.py
@@ -239,7 +239,8 @@ class FileManager(Manager):
         enable_modes = os.path.getsize(path) < self._limit
         for m in self.editor.modes:
             m.enabled = enable_modes
-        self.editor.modes.clear()
+        if not enable_modes:
+            self.editor.modes.clear()
         # open file and get its content
         try:
             with open(path, 'Ur', encoding=encoding) as file:
