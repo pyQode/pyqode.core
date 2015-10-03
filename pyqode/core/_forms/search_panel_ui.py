6c3b489d31f9ff4959b6aab50e95df52b4afc6a3
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-10-03 15:19:53 +0200

    Search/Replace panel: add tooltips
    See OpenCobolIDE/OpenCobolIDE#176

--- pyqode/core/_forms/search_panel_ui.py
+++ pyqode/core/_forms/search_panel_ui.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/colin/Documents/pyqode/core/forms/search_panel.ui'
+# Form implementation generated from reading ui file '/home/colin/work/pyqode.core/forms/search_panel.ui'
 #
-# Created: Sun Oct 12 17:19:30 2014
-#      by: PyQt5 UI code generator 5.3.2
+# Created by: PyQt5 UI code generator 5.5
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,19 +11,19 @@ from pyqode.qt import QtCore, QtGui, QtWidgets
 class Ui_SearchPanel(object):
     def setupUi(self, SearchPanel):
         SearchPanel.setObjectName("SearchPanel")
-        SearchPanel.resize(732, 90)
+        SearchPanel.resize(884, 90)
         SearchPanel.setStyleSheet("")
         self.verticalLayout = QtWidgets.QVBoxLayout(SearchPanel)
-        self.verticalLayout.setSpacing(0)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName("verticalLayout")
         self.frame = QtWidgets.QFrame(SearchPanel)
         self.frame.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.frame)
-        self.verticalLayout_2.setSpacing(9)
         self.verticalLayout_2.setContentsMargins(9, 9, 9, 9)
+        self.verticalLayout_2.setSpacing(9)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.widgetSearch = QtWidgets.QWidget(self.frame)
         self.widgetSearch.setObjectName("widgetSearch")
@@ -119,6 +118,10 @@ class Ui_SearchPanel(object):
         self.horizontalLayout_2.addWidget(self.toolButtonReplaceAll)
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem1)
+        self.lineEditReplace.raise_()
+        self.toolButtonReplace.raise_()
+        self.toolButtonReplaceAll.raise_()
+        self.labelReplace.raise_()
         self.verticalLayout_2.addWidget(self.widgetReplace)
         self.verticalLayout.addWidget(self.frame)
         self.actionSearch = QtWidgets.QAction(SearchPanel)
@@ -156,11 +159,20 @@ class Ui_SearchPanel(object):
     def retranslateUi(self, SearchPanel):
         _translate = QtCore.QCoreApplication.translate
         SearchPanel.setWindowTitle(_translate("SearchPanel", "Form"))
+        self.lineEditSearch.setToolTip(_translate("SearchPanel", "Search term"))
+        self.toolButtonPrevious.setToolTip(_translate("SearchPanel", "Select previous occurence"))
+        self.toolButtonNext.setToolTip(_translate("SearchPanel", "Select next occurence"))
+        self.checkBoxRegex.setToolTip(_translate("SearchPanel", "Use a regular expression for search occurences"))
         self.checkBoxRegex.setText(_translate("SearchPanel", "Regex"))
+        self.checkBoxCase.setToolTip(_translate("SearchPanel", "Enable case sensitive search"))
         self.checkBoxCase.setText(_translate("SearchPanel", "Match case"))
+        self.checkBoxWholeWords.setToolTip(_translate("SearchPanel", "Search for whole words only"))
         self.checkBoxWholeWords.setText(_translate("SearchPanel", "Whole words"))
         self.labelMatches.setText(_translate("SearchPanel", "0 matches"))
+        self.lineEditReplace.setToolTip(_translate("SearchPanel", "Replacement text"))
+        self.toolButtonReplace.setToolTip(_translate("SearchPanel", "Replace current occurence"))
         self.toolButtonReplace.setText(_translate("SearchPanel", "Replace"))
+        self.toolButtonReplaceAll.setToolTip(_translate("SearchPanel", "Replace all occurences"))
         self.toolButtonReplaceAll.setText(_translate("SearchPanel", "Replace All"))
         self.actionSearch.setText(_translate("SearchPanel", "Search"))
         self.actionSearch.setToolTip(_translate("SearchPanel", "Show the search panel"))
