d14ed4dd09f7c9cac1f71649ecf94a6a676e53e9
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-11-05 12:23:39 +0100

    Give a name to our custom log levels

--- pyqode/core/__init__.py
+++ pyqode/core/__init__.py
@@ -7,5 +7,11 @@ It contains the base classes for both the backend and the frontend and provides
 a series of modes and panels that might be useful for any kind of code editor
 widget, i.e. pyqode.core is a generic code editor widget.
 """
+import logging
+
 
 __version__ = '2.7.0.dev33'
+
+
+logging.addLevelName(1, "PYQODEDEBUGCOMM")
+logging.addLevelName(5, "PYQODEDEBUG")
