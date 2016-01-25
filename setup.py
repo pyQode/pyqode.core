c379f41e489bb6f785f8afc61f1e28d9c2d14865
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2016-01-25 10:19:24 +0100

    Tests: enable xdist --boxed mode
    Each test will now run in a separated subprocess

--- setup.py
+++ setup.py
@@ -21,22 +21,28 @@ try:
 except ImportError:
     cmdclass = {}
 
-class PyTest(TestCommand):
-    """
-    Overrides setup "test" command, taken from here:
-    http://pytest.org/latest/goodpractises.html
-    """
 
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
-
-        errno = pytest.main([])
+        # force the use of boxed mode (each test will run in an isolated
+        # process)
+        if self.pytest_args:
+            self.pytest_args = self.pytest_args.replace('"', '').split(' ')
+        else:
+            self.pytest_args = []
+        if '--boxed' not in self.pytest_args:
+            self.pytest_args.insert(0, '--boxed')
+        print('running test command: py.test "%s"' % ' '.join(
+            self.pytest_args))
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 cmdclass['test'] = PyTest
@@ -70,6 +76,7 @@ setup(
     description=DESCRIPTION,
     long_description=readme(),
     install_requires=[pygments_req, 'pyqode.qt', 'future'],
+    tests_require=['pytest-xdist', 'pytest-cov', 'pytest'],
     entry_points={
         'console_scripts': [
             'pyqode-console = pyqode.core.tools.console:main'
