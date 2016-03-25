18d1a63906f1b2731619e0945648a8ef84c2ccd3
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-03-25 09:17:43 +0100

    Fix file permission lost on save on posix

--- pyqode/core/managers/file.py
+++ pyqode/core/managers/file.py
@@ -375,6 +375,13 @@ class FileManager(Manager):
             encoding = self._encoding
         self.saving = True
         self.editor.text_saving.emit(str(path))
+
+        # get file persmission on linux
+        try:
+            st_mode = os.stat(path).st_mode
+        except (ImportError, TypeError, AttributeError):
+            st_mode = None
+
         # perform a safe save: we first save to a temporary file, if the save
         # succeeded we just rename the temporary file to the final file name
         # and remove it.
@@ -411,6 +418,12 @@ class FileManager(Manager):
             self.saving = False
             _logger().debug('file saved: %s', path)
             self._check_for_readonly()
+
+            # restore file permission
+            try:
+                os.chmod(path, st_mode)
+            except (ImportError, TypeError, AttributeError):
+                pass
 
     def close(self, clear=True):
         """
