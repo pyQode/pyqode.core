a082ff2165044692f3380d1b1592000690c2ba5b
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-03-30 14:51:24 +0200

    Fix runtime error if parent deleted before _on_path_loaded has been called

--- pyqode/core/widgets/filesystem_treeview.py
+++ pyqode/core/widgets/filesystem_treeview.py
@@ -235,16 +235,21 @@ class FileSystemTreeView(QtWidgets.QTreeView):
     def _on_path_loaded(self, path):
         if os.path.normpath(path) != self._root_path:
             return
-        self.setModel(self._fs_model_proxy)
-        file_root_index = self._fs_model_source.setRootPath(self._root_path)
-        root_index = self._fs_model_proxy.mapFromSource(file_root_index)
-        self.setRootIndex(root_index)
-        if not os.path.ismount(self._root_path):
-            self.expandToDepth(0)
-        if self._hide_extra_colums:
-            self.setHeaderHidden(True)
-            for i in range(1, 4):
-                self.hideColumn(i)
+        try:
+            self.setModel(self._fs_model_proxy)
+            file_root_index = self._fs_model_source.setRootPath(
+                self._root_path)
+            root_index = self._fs_model_proxy.mapFromSource(file_root_index)
+            self.setRootIndex(root_index)
+            if not os.path.ismount(self._root_path):
+                self.expandToDepth(0)
+            if self._hide_extra_colums:
+                self.setHeaderHidden(True)
+                for i in range(1, 4):
+                    self.hideColumn(i)
+        except RuntimeError:
+            # wrapped C/C++ object of type FileSystemTreeView has been deleted
+            return
 
     def filePath(self, index):
         """
