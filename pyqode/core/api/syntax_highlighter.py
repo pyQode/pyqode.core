6a8e031ca108bd5fcda696f577b3e5a190108392
Sebastiaan Mathot <s.mathot@cogsci.nl>
2019-09-20 08:50:12 +0200

    SyntaxHighlighter: don't unnecessarily call _reset_stylesheet() - Improves performance

--- pyqode/core/api/syntax_highlighter.py
+++ pyqode/core/api/syntax_highlighter.py
@@ -254,6 +254,7 @@ class SyntaxHighlighter(QtGui.QSyntaxHighlighter, Mode):
 
         :param color_scheme: new color scheme.
         """
+        self.editor._auto_reset_stylesheet = False
         self.editor.background = color_scheme.background
         self.editor.foreground = color_scheme.formats[
             'normal'].foreground().color()
@@ -272,6 +273,7 @@ class SyntaxHighlighter(QtGui.QSyntaxHighlighter, Mode):
             pass
         else:
             mode.refresh_decorations(force=True)
+        self.editor._auto_reset_stylesheet = True
         self.editor._reset_stylesheet()
 
     def __init__(self, parent, color_scheme=None):
